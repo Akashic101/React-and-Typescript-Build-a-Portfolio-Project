65301facb47c65eb6e1cb62d8df08c8132692f40
David Moll <david.leander.moll@gmail.com>
2022-01-17 11:14:33 +0100

    20: Adding an auto-focus effect using useEffect()

--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useReducer } from "react";
+import { useState, useRef, useReducer, useEffect } from "react";
 
 const users = [
 	{ name: "David", age: 23 },
@@ -9,6 +9,22 @@ const UserSearch: React.FC = () => {
 	const inputRef = useRef<HTMLInputElement | null>(null); //Create ref with current value "null" and will be an InputElement or Null
 	const [name, setName] = useState("");
 	const [user, setUser] = useState<{ name: string; age: number } | undefined>(); //A user can only be of type User or undefined, so we use a Union-type
+
+
+	/*
+	With useEffect we can apply effects like auto-focus on an element.
+	We do need to check if that element exists before since it could be null
+
+	TODO IF-statement vs inputRef.current?.focus();
+
+	*/
+
+	useEffect(() => {
+		if(!inputRef.current) {
+			return;
+		}
+		inputRef.current.focus();
+	}, []) //Makes sure this effect can only exist once
 
 	const onClick = () => {
 		const foundUser = users.find((user) => {
